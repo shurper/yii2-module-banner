9bce76a8ce1f8a04ad288801124e5bac38ccb510
floor12 <floor12@floor12.net>
2019-02-26 11:43:22 +0100

    Lost migration added

---
+++ src/migrations/m190226_102147_add_archive_option.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190226_102147_add_archive_option
+ */
+class m190226_102147_add_archive_option extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('{{ads_banner}}', 'archive', $this->boolean()->defaultValue(0)->comment('Архивный'));
+        $this->addColumn('{{ads_popup}}', 'archive', $this->boolean()->defaultValue(0)->comment('Архивный'));
+
+        $this->createIndex('idx-ads_banner-archive', '{{ads_banner}}', 'archive');
+        $this->createIndex('idx-ads_popup-archive', '{{ads_banner}}', 'archive');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{ads_banner}}', 'archive');
+        $this->dropTable('{{ads_popup}}', 'archive');
+    }
+
+}
