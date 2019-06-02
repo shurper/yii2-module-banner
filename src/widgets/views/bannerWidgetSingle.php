dcade24ba3d967e9d3e82938c9f691a10913a957
floor12 <floor12@floor12.net>
2019-06-02 17:18:22 +0200

    Empty mobile banner version bug fixed.

--- src/widgets/views/bannerWidgetSingle.php
+++ src/widgets/views/bannerWidgetSingle.php
@@ -18,13 +18,22 @@ BannerAsset::register($this);
 
 
 if ($banners->type == AdsBanner::TYPE_IMAGE)
-    $img = "<picture>
+
+
+    if ($banners->file_mobile)
+        $img = "<picture>
                 <source type='image/webp' media='(min-width: 700px)' srcset='{$banners->file_desktop->getPreviewWebPath(1920,0,true)} 1x, {$banners->file_desktop->getPreviewWebPath(3840,true)} 2x'>              
                 <source type='image/webp' media='(max-width: 700px)' srcset='{$banners->file_mobile->getPreviewWebPath(700,true)} 1x, {$banners->file_mobile->getPreviewWebPath(1400,true)} 2x'>
                 <source type='{$banners->file_desktop->content_type}' media='(min-width: 700px)' srcset='{$banners->file_desktop->getPreviewWebPath(1920)} 1x, {$banners->file_desktop->getPreviewWebPath(3840)} 2x'>
                 <source type='{$banners->file_desktop->content_type}' srcset='{$banners->file_mobile->getPreviewWebPath(700)} 1x, {$banners->file_mobile->getPreviewWebPath(1400)} 2x'>
                 <img src='{$banners->file_desktop->getPreviewWebPath(1920)}' class='img-responsive' alt='{$banners->title}'>
-            </picture>"; 
+            </picture>";
+    else
+        $img = "<picture>
+                <source type='image/webp' media='(min-width: 700px)' srcset='{$banners->file_desktop->getPreviewWebPath(1920,0,true)} 1x, {$banners->file_desktop->getPreviewWebPath(3840,true)} 2x'>              
+                <source type='{$banners->file_desktop->content_type}' media='(min-width: 700px)' srcset='{$banners->file_desktop->getPreviewWebPath(1920)} 1x, {$banners->file_desktop->getPreviewWebPath(3840)} 2x'>
+                <img src='{$banners->file_desktop->getPreviewWebPath(1920)}' class='img-responsive' alt='{$banners->title}'>
+            </picture>";
 else
     $img = Html::tag('iframe', null, [
         'src' => $banners->webPath,
--- src/widgets/views/bannerWidgetSlider.php
+++ src/widgets/views/bannerWidgetSlider.php
@@ -40,11 +40,18 @@ echo "<div id='{$id}'>";
 foreach ($banners as $banner) {
 
     if ($banner->type == AdsBanner::TYPE_IMAGE)
-        $img = "<picture>
+        if ($banner->file_mobile)
+            $img = "<picture>
                 <source type='image/webp' media='(min-width: 700px)' srcset='{$banner->file_desktop->getPreviewWebPath(1920, 0, true)} 1x, {$banner->file_desktop->getPreviewWebPath(3840,0, true)} 2x'>              
                 <source type='image/webp' media='(max-width: 700px)' srcset='{$banner->file_mobile->getPreviewWebPath(700, 0, true)} 1x, {$banner->file_mobile->getPreviewWebPath(1400,0, true)} 2x'>
                 <source type='{$banner->file_desktop->content_type}' media='(min-width: 700px)' srcset='{$banner->file_desktop->getPreviewWebPath(1920)} 1x, {$banner->file_desktop->getPreviewWebPath(3840)} 2x'>
                 <source type='{$banner->file_desktop->content_type}' srcset='{$banner->file_mobile->getPreviewWebPath(700)} 1x, {$banner->file_mobile->getPreviewWebPath(1400)} 2x'>
+                <img src='{$banner->file_desktop->getPreviewWebPath(1920)}' class='img-responsive' alt='{$banner->title}'>
+            </picture>";
+        else
+            $img = "<picture>
+                <source type='image/webp' media='(min-width: 700px)' srcset='{$banner->file_desktop->getPreviewWebPath(1920, 0, true)} 1x, {$banner->file_desktop->getPreviewWebPath(3840,0, true)} 2x'>              
+                <source type='{$banner->file_desktop->content_type}' media='(min-width: 700px)' srcset='{$banner->file_desktop->getPreviewWebPath(1920)} 1x, {$banner->file_desktop->getPreviewWebPath(3840)} 2x'>
                 <img src='{$banner->file_desktop->getPreviewWebPath(1920)}' class='img-responsive' alt='{$banner->title}'>
             </picture>";
 
